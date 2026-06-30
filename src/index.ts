b8ab3cd0a8afc4adc04e5b84a26b2b7f7510c77d
pritamduttaofficial <pritamdutta850@gmail.com>
2026-06-30 13:50:15 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
