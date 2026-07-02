5f0ae95c9d60f926a2d7049b8c4ea7b48c890416
pritamduttaofficial <pritamdutta850@gmail.com>
2026-07-02 10:59:07 +0530

    db schema

---
+++ drizzle/20260702050020_rich_rick_jones/migration.sql
@@ -0,0 +1,20 @@
+CREATE TABLE "departments" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "departments_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"code" varchar(50) NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	"description" varchar(500),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "subjects" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "subjects_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"department_id" integer NOT NULL,
+	"code" varchar(50) NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	"description" varchar(500),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "subjects" ADD CONSTRAINT "subjects_department_id_departments_id_fkey" FOREIGN KEY ("department_id") REFERENCES "departments"("id") ON DELETE RESTRICT;
